11773565102ae64c1176a665f6be4d43d590ecb3
artica2005 <artica2005@gmail.com>
2023-01-27 16:01:53 +0700

    1

--- index.php
+++ index.php
@@ -13,8 +13,9 @@
     <title>ระบบตะกร้าสั่งซื้อสินค้าอย่างง่าย appzstory.dev</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt">
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt"> -->
+    <link rel="stylesheet" href="asset/style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
